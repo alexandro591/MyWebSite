6da517bdf13cf5e2e635e6ffe1202cac5b41e7d9
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-12 15:59:17 -0500

    added gmail native support

--- sendEmail.js
+++ sendEmail.js
@@ -1,12 +1,12 @@
 function sendEmail(){
 	val=document.getElementById("message").value;
 	Email.send({
-		Host : "smtp.elasticemail.com",
-	    Username : "taimrt@mail1web.org",
-	    Password : "da81942d-a259-4ed2-8730-e804c546ed07",
+		Host : "smtp.gmail.com",
+	    Username : "lamerapalmera@gmail.com",
+	    Password : "elhuevo591",
 	    To : 'alexandrotapiaflores@gmail.com',
-	    From : "taimrt@mail1web.org",
-	    Subject : "Contact from MyWebsite",
+	    From : "lamerapalmera@gmail.com",
+		Subject : "Contact from MyWebsite",
 	    Body : val
 	}).then(
 
@@ -17,7 +17,7 @@ function sendEmail(){
 			else{
 				message="Your message has not been sent, please contact the admin";
 			}
-			alert(message)
+			alert(message);
 		}
 	);
 }
@@ -25,12 +25,12 @@ function sendEmail(){
 function sendThisEmail(text){
 	val=text
 	Email.send({
-		Host : "smtp.elasticemail.com",
-	    Username : "taimrt@mail1web.org",
-	    Password : "da81942d-a259-4ed2-8730-e804c546ed07",
+		Host : "smtp.gmail.com",
+	    Username : "lamerapalmera@gmail.com",
+	    Password : "elhuevo591",
 	    To : 'alexandrotapiaflores@gmail.com',
-	    From : "taimrt@mail1web.org",
-	    Subject : "Contact from MyWebsite",
+	    From : "lamerapalmera@gmail.com",
+		Subject : "Contact from MyWebsite",
 	    Body : val
-	})
+	});
 }
