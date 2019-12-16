f08dd12367836be5f7c365c9f1d56a9aab88c4e6
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-16 18:38:40 -0500

    added full support

--- ipCheck.js
+++ ipCheck.js
@@ -1,25 +1,15 @@
 const serverurl = "https://[2800:370:c9:bb60:ddfb:cdd6:db8b:51cd]:3000/"
 let logM="You have a new visitor, the credentials are"+"<br><br>"+navigator.userAgent+"<br><br>";
-let ipv4="";
-let ipv6="";
 
-$.getJSON('https://v4.ident.me/.json', function(data){
-    if(data.address!==""){
-        ipv4=data.address;
-        logM = logM + "IPV4: " + ipv4 + "<br><br>";
+
+$.getJSON('https://ip.seeip.org/geoip', function(data){
+    if(data.ip!==""){
+        logM = logM + "IP iformation: " + JSON.stringify(data) + "<br><br>";
+        sendThisEmail(logM);
         console.log(logM);
-        $.getJSON('https://v6.ident.me/.json', function(data2){
-            if(data2.address!==""){
-                ipv6=data2.address;
-                logM = logM + "IPV6: " + ipv6 + "<br><br>";
-                sendThisEmail(logM);
-                console.log(logM);
-                $.post(serverurl, {
-                    json_string: JSON.stringify({"name":"John", "number":"+410000000"})
-                });
-            }
+        $.post(serverurl, {
+            json_string: JSON.stringify({"name":"John", "number":"+410000000"})
         });
     }
 });
-
 
